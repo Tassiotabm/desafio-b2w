d98e964a69a229603b4c1b1b6cf50c672d115ffa
Tassio <tabm2010@gmail.com>
2019-01-17 12:40:31 -0200

    change list Return

--- controller/planetController.js
+++ controller/planetController.js
@@ -48,7 +48,10 @@ router.get('/', function (req, res) {
                 })
             });
         }
-        res.status(200).send(response);
+        res.status(200).send({
+            Planets: response,
+            Count: response.length
+        });
     });
 });
 
