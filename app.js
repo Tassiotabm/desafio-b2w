7e51ab39f077c5dbbd1d1ac687901e8f5235f828
Tassio <tabm2010@gmail.com>
2019-01-18 23:21:24 -0200

    fix module

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
 var db = require('./db');
-
-var swapi = require('./util/Swapi');
 var PlanetController = require('./controller/planetController');
 app.use('/planet', PlanetController);
 
